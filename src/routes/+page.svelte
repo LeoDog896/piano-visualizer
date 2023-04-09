643c2709780a4063707700d6f9f5a386ae966024
Tristan F <LeoDog896@hotmail.com>
2023-04-09 10:45:52 -0400

    feat: initial midi file reading

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import * as midiManager from 'midi-file';
+
+    let files: FileList | undefined;
+    let midi: midiManager.MidiData | undefined;
+
+    function load(event: ProgressEvent<FileReader>) {
+        if (!event.target) return;
+        const array = event.target.result;
+        if (!array) return;
+
+        // files can either be a string or an array of bytes - coersion is needed
+        const parsedArray = typeof array === 'string'
+            ? new Uint8Array([...array].map((c) => c.charCodeAt(0)))
+            : new Uint8Array(array);
+        midi = midiManager.parseMidi(parsedArray);
+
+        console.log(midi);
+    }
+
+    $: if (files && files.length > 0) {
+        const reader = new FileReader();
+        reader.addEventListener("load", load);
+        reader.readAsArrayBuffer(files[0]);
+    }
+</script>
+
+<input type="file" accept=".mid" bind:files={files} />
+
+{#if files}
+    {#each files as file}
+        <p>{file.name}</p>
+    {/each}
+{/if}
+
+{#if midi}
+    <p>{midi.header}</p>
+{/if}
