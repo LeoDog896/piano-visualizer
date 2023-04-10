0d48f9fd97f6bd518545840be177cd04013c5caf
Tristan F <LeoDog896@hotmail.com>
2023-04-10 14:58:40 -0400

    feat: allow different markers
    this also switches octaves to be the default marker

--- src/lib/Piano.svelte
+++ src/lib/Piano.svelte
@@ -6,6 +6,7 @@
 	let container: HTMLDivElement;
 
 	export let octaves = [0, 1, 2, 3, 4, 5, 6, 7];
+    export let markers: 'octaves' | 'notes' | 'none' = 'octaves';
 
 	const notes = [
 		['A0', 'A#0', 'B0'],
@@ -41,12 +42,16 @@
 				tone.triggerAttackRelease(note, '8n');
 			}}
 		>
-			{#if note.includes('#')}
-				<span>{note.replace('#', '♯').replace(/\d/, '')}</span><br />
-				<span>{note.slice(-1)}</span>
-			{:else}
-				{note}
-			{/if}
+            {#if markers == "notes"}
+                {#if note.includes('#')}
+                    <span>{note.replace('#', '♯').replace(/\d/, '')}</span><br />
+                    <span>{note.slice(-1)}</span>
+                {:else}
+                    <span>{note}</span>
+                {/if}
+            {:else if markers == "octaves" && note.match(/C\d/)}
+                <span>C{note.slice(-1)}</span>
+            {/if}
 		</button>
 	{/each}
 </div>
@@ -57,19 +62,28 @@
 		--width: calc(100% / var(--non-sharp-notes));
 	}
 
+    span {
+        position: absolute;
+        bottom: 0;
+        transform: translate(-50%, -50%);
+    }
+
 	button {
+        position: relative;
 		width: var(--width);
 		height: 10rem;
 		margin: 0;
 		padding: 0;
+        text-align: center;
 		overflow-wrap: break-word;
+        transition: all 0.1s ease-in-out;
 	}
 
 	button.white {
 		box-shadow: -1px 0 0 rgba(255, 255, 255, 0.8) inset, 0 0 5px #ccc inset,
 			0 0 3px rgba(0, 0, 0, 0.2);
 		background: linear-gradient(to bottom, #eee 0%, #fff 100%);
-		border-left: 1px solid #bbb;
+		border-left: 0px solid #bbb;
 		border-bottom: 1px solid #bbb;
 		border-radius: 0 0 5px 5px;
 	}
@@ -87,6 +101,7 @@
 		box-shadow: -1px -1px 2px rgba(255, 255, 255, 0.2) inset,
 			0 -5px 2px 3px rgba(0, 0, 0, 0.6) inset, 0 2px 4px rgba(0, 0, 0, 0.5);
 		background: linear-gradient(45deg, #222 0%, #555 100%);
+        z-index: 1;
 	}
 
 	.black:active {
@@ -96,9 +111,6 @@
 	}
 
 	.white:active {
-		border-top: 1px solid #777;
-		border-left: 1px solid #999;
-		border-bottom: 1px solid #999;
 		box-shadow: 2px 0 3px rgba(0, 0, 0, 0.1) inset, -5px 5px 20px rgba(0, 0, 0, 0.2) inset,
 			0 0 3px rgba(0, 0, 0, 0.2);
 		background: linear-gradient(to bottom, #fff 0%, #e9e9e9 100%);
