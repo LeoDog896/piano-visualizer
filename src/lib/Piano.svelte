66a55c176afccb1f0ed1bee1212a38e6be2ddc77
Tristan F <LeoDog896@hotmail.com>
2023-04-10 13:43:16 -0400

    feat: black key hover effect

--- src/lib/Piano.svelte
+++ src/lib/Piano.svelte
@@ -3,8 +3,8 @@
 	import * as Tone from 'tone';
 
 	let tone: Tone.Synth;
-    let container: HTMLDivElement;
-    
+	let container: HTMLDivElement;
+
 	export let octaves = [0, 1, 2, 3, 4, 5, 6, 7];
 
 	const notes = [
@@ -20,9 +20,9 @@
 	];
 
 	$: chosenNotes = notes.slice(0, octaves.length).flat();
-    $: nonSharpNotes = chosenNotes.filter((note) => !note.includes('#'));
+	$: nonSharpNotes = chosenNotes.filter((note) => !note.includes('#'));
 
-    $: container && container.style.setProperty('--non-sharp-notes', nonSharpNotes.length.toString());
+	$: container && container.style.setProperty('--non-sharp-notes', nonSharpNotes.length.toString());
 
 	onMount(async () => {
 		tone = new Tone.Synth().toDestination();
@@ -34,20 +34,27 @@
 </script>
 
 <div bind:this={container}>
-    {#each chosenNotes as note}
-        <button
-            class={note.includes('#') ? 'black' : 'white'}
-            on:click={() => {
-                tone.triggerAttackRelease(note, '8n');
-            }}
-        />
-    {/each}
+	{#each chosenNotes as note}
+		<button
+			class={note.includes('#') ? 'black' : 'white'}
+			on:click={() => {
+				tone.triggerAttackRelease(note, '8n');
+			}}
+		>
+			{#if note.includes('#')}
+				<span>{note.replace('#', '♯').replace(/\d/, '')}</span><br />
+				<span>{note.slice(-1)}</span>
+			{:else}
+				{note}
+			{/if}
+		</button>
+	{/each}
 </div>
 
 <style lang="scss">
 	div {
-        --non-sharp-notes: 51;
-		--width: calc(100vw / var(--non-sharp-notes));
+		--non-sharp-notes: 51;
+		--width: calc(100% / var(--non-sharp-notes));
 	}
 
 	button {
@@ -55,13 +62,14 @@
 		height: 10rem;
 		margin: 0;
 		padding: 0;
+		overflow-wrap: break-word;
 	}
 
 	button.white {
 		box-shadow: -1px 0 0 rgba(255, 255, 255, 0.8) inset, 0 0 5px #ccc inset,
 			0 0 3px rgba(0, 0, 0, 0.2);
 		background: linear-gradient(to bottom, #eee 0%, #fff 100%);
-		border-left: 1 px solid #bbb;
+		border-left: 1px solid #bbb;
 		border-bottom: 1px solid #bbb;
 		border-radius: 0 0 5px 5px;
 	}
@@ -77,8 +85,8 @@
 		border: 1px solid #000;
 		border-radius: 0 0 3px 3px;
 		box-shadow: -1px -1px 2px rgba(255, 255, 255, 0.2) inset,
-			0 -2px 2px 3px rgba(0, 0, 0, 0.6) inset, 0 1px 2px rgba(0, 0, 0, 0.5);
-		background: linear-gradient(to right, #444 0%, #222 100%);
+			0 -5px 2px 3px rgba(0, 0, 0, 0.6) inset, 0 2px 4px rgba(0, 0, 0, 0.5);
+		background: linear-gradient(45deg, #222 0%, #555 100%);
 	}
 
 	.black:active {
---
+++ src/routes/piano/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Piano from '$lib/Piano.svelte';
+</script>
+
+<div>
+	<Piano />
+</div>
+
+<style>
+	/* move piano to bottom of page */
+	div {
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		right: 0;
+	}
+</style>
